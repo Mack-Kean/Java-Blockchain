5d6e73539240eb3633c106ecf1792c251e33470f
MacKenzie Kean <mkean01@uoguelph.ca>
2021-01-02 17:04:43 -0500

    Created a wrapper class for the main method of the simulator so the Network class is standalone

---
+++ src/main/java/blockchain/NetworkSim.java
@@ -0,0 +1,65 @@
+package blockchain;
+
+import java.util.ArrayList;
+/**
+ * A wrapper class used to create a command line demonstration of the proof-of-work blockchain.
+ */
+public class NetworkSim {
+
+    /**
+     * The main class for the blockchain proof of concept.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        BlockChain bc = new BlockChain();
+        Node node = new Node();
+        Block proposedBlock;
+
+        Miner blockMiner = new Miner("Initial Transaction", "0");
+        proposedBlock = blockMiner.getNewestBlock(); //this is where the actual block is mined
+        node.updateNode(bc, proposedBlock);
+        if (node.isChainValidated()) {
+            System.out.println("Genesis Node validated");
+            bc = node.getProposedBlockChain();
+        }
+    
+        blockMiner.newTransaction("Second Transaction", bc.getLastBlockHash());
+        proposedBlock = blockMiner.getNewestBlock(); //this is where the actual block is mined
+        node.updateNode(bc, proposedBlock);
+        if (node.isChainValidated()) {
+            System.out.println("Second Node validated");
+            bc = node.getProposedBlockChain();
+        }
+    
+        blockMiner.newTransaction("Third Transaction", bc.getLastBlockHash());
+        proposedBlock = blockMiner.getNewestBlock(); //this is where the actual block is mined
+        node.updateNode(bc, proposedBlock);
+        if (node.isChainValidated()) {
+            System.out.println("Third Node validated");
+            bc = node.getProposedBlockChain();
+        }
+
+        blockMiner.newTransaction("Faulty Transaction", bc.getLastBlockHash());
+        proposedBlock = blockMiner.getNewestBlock(); //this is where the actual block is mined
+
+        proposedBlock.setData("Changed Transaction Data"); //data has been tampered with
+
+        node.updateNode(bc, proposedBlock);
+        if (node.isChainValidated()) {
+            System.out.println("Third Node validated");
+            bc = node.getProposedBlockChain();
+        } else {
+            System.out.println("Block has been rejected and not added to the chain");
+        }
+
+        ArrayList<Block> chainTester = bc.getChain();
+
+        for (Block b : chainTester) {
+            System.out.println("Hash Value: " + b.getHash());
+            System.out.println("Previous Hash Value: " + b.getPreviousHash());
+            System.out.println("Nonce Value: " + b.getNonce()); //shows how much work was needed to mine the block
+        }
+
+    }
+
+}
